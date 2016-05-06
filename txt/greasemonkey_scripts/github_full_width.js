3cea52a104195b58afe7061e1e7615f58cd68242
HybridDog <ovvv@web.de>
2016-05-06 17:12:44 +0200

    update github size script to my version

--- txt/greasemonkey_scripts/github_full_width.js
+++ txt/greasemonkey_scripts/github_full_width.js
@@ -1,18 +1,34 @@
 ﻿// ==UserScript==
-// @name           Make GitHub Pull Request, Commit, and Blob pages full width
-// @namespace      https://bitbucket.org/deadlydog/greasemonkeyscripts
-// @description    Makes the GitHub Pull Request, Commit, and Blob pages span the full width of the browser, rather than maxing out at the default ~900 pixels.
-// @include        https://github.com/*/pull/*/files*
-// @include        https://github.com/*/commit/*
-// @include        https://github.com/*/blob/*
+// @name           Make GitHub pages full width
+// @namespace      https://bitbucket.org/deadlydog/greasemonkeyscripts, edited by HybridDog
+// @description    Makes the GitHub pages span the full width of the browser, rather than maxing out at the default ~900 pixels.
+// @include        https://github.com*
 // @grant          none
-// @version        1.6.1
+// @version        1.6.1, edit 2016.05.06
 // ==/UserScript==
 
 elements = document.getElementsByClassName('container');
 for (index = 0; index < elements.length; index++)
 {
 	elements[index].style.width="95%";	// Only 95% to leave room for the "add comment" tooltip icon.
+}
+
+elements = document.getElementsByClassName('comment-body');
+for (index = 0; index < elements.length; index++)
+{
+	elements[index].style["font-size"]="130%";	// Bigger comment font size
+}
+
+elements = document.getElementsByClassName('discussion-timeline');
+for (index = 0; index < elements.length; index++)
+{
+	elements[index].style.width="77%";	// Comments left to 77%
+}
+
+elements = document.getElementsByClassName('discussion-sidebar');
+for (index = 0; index < elements.length; index++)
+{
+	elements[index].style.width="20%";	// Suscribe button etc. right from 20%
 }
 
 elements = document.getElementsByClassName('repository-content context-loader-container');
