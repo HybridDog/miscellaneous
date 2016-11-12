c9f8461f0ad42883320d6c8263e6d73fe5917837
HybridDog <ovvv@web.de>
2016-11-12 21:31:42 +0100

    grey messages work, try using trap to avoid keeping the terminal grey after aborting
    M  txt/akomfort_modpack/modpack_dl.sh

--- txt/akomfort_modpack/modpack_dl.sh
+++ txt/akomfort_modpack/modpack_dl.sh
@@ -4,6 +4,7 @@ echo "Creating folder if not existent…"
 mkdir -p akomfort
 cd akomfort
 echo "Download or update mods…"
+trap "echo -e -n '\033[0m'" SIGHUP SIGINT SIGTERM
 echo -e -n "\033[2m"
 curl -s https://raw.githubusercontent.com/HybridDog/miscellaneous/master/txt/akomfort_modpack/generated_commands | xargs -P0 -L1 -I {} sh -c "{}"
 echo -e -n "\033[0m"
