3ef323afcb2ea4c1b1cfbee4f295d8098ddb9987
HybridDog <ovvv@web.de>
2023-09-09 15:37:23 +0200

    Update links in the Minecraft to Minetest texture pack linker
    The script no longer works with new Minecraft texture packs,
    so the link paths need to be updated.
    
    * The `blocks` and `items` folder are now named `block` and `item`
      (see https://minecraft.fandom.com/wiki/Resource_pack#Folder_structure)
    * Some textures in `minetest_game` have been renamed
    * File names in Minecraft texture packs have changed,
      e.g. suffix and prefix switches such as 'log_jungle' -> 'jungle_log'
    * Links from gold to mese textures are wrong.
      Mese is a SciFi material and gold is not.
    
    The links are still not all up to date,
    for example door and crops links probably do not work.

--- txt/make_links.sh
+++ txt/make_links.sh
@@ -30,13 +30,13 @@ link() {
 # a linking function for blocks
 link_block() {
 	#echo "link_block $1 $2"
-	link ${textures_path}/blocks/$1 $2
+	link ${textures_path}/block/$1 $2
 }
 
 # a linking function for items
 link_item() {
 	#echo "link_item $1 $2"
-	link ${textures_path}/items/$1 $2
+	link ${textures_path}/item/$1 $2
 }
 
 echo "linking textures…"
@@ -45,7 +45,7 @@ link ${mctp_path}/pack screenshot
 
 link_item apple default_apple
 link_item arrow throwing_arrow
-link_item boat boat_inventory
+link_item boat boats_inventory
 link_item book default_book
 link_item book_normal default_book
 link_item bow_standby throwing_bow_wood
@@ -53,11 +53,14 @@ link_item bread farming_bread
 link_item brick default_clay_brick
 link_item bucket bucket
 link_item bucket_empty bucket
+link_item lava_bucket bucket_lava
 link_item bucket_lava bucket_lava
 link_item bucketLava bucket_lava
+link_item water_bucket bucket_water
 link_item bucket_water bucket_water
 link_item bucketWater bucket_water
 link_item clay default_clay_lump
+link_item clay_ball default_clay_lump
 link_item coal default_coal_lump
 link_item diamond_axe default_tool_diamondaxe
 link_item diamond_boots 3d_armor_inv_boots_diamond
@@ -69,12 +72,27 @@ link_item diamond_shovel default_tool_diamondshovel
 link_item diamond_sword default_tool_diamondsword
 link_item doorIron door_steel
 link_item doorWood door_wood
+link_item black_dye dye_black
+link_item blue_dye dye_blue
+link_item brown_dye dye_brown
+link_item cyan_dye dye_cyan
+link_item green_dye dye_dark_green
+link_item gray_dye dye_dark_grey
+link_item lime_dye dye_green
+link_item magenta_dye dye_magenta
+link_item orange_dye dye_orange
+link_item pink_dye dye_pink
+link_item purple_dye dye_violet
+link_item red_dye dye_red
+link_item light_gray_dye dye_grey
+link_item white_dye dye_white
+link_item yellow_dye dye_yellow
 link_item dyePowder_black dye_black
-link_item dyePowder_blua dye_blua
+link_item dyePowder_blue dye_blue
 link_item dyePowder_brown dye_brown
 link_item dyePowder_cyan dye_cyan
 link_item dyePowder_green dye_dark_green
-link_item dyePowder_grey dye_dark_grey
+link_item dyePowder_gray dye_dark_grey
 link_item dyePowder_lime dye_green
 link_item dyePowder_magenta dye_magenta
 link_item dyePowder_orange dye_orange
@@ -87,7 +105,6 @@ link_item dyePowder_yellow dye_yellow
 link_item glassBottle vessels_glass_bottle
 link_item goldNugget default_gold_lump
 link_item hatchetDiamond default_tool_diamondaxe
-link_item hatchetGold default_tool_meseaxe
 link_item hatchetIron default_tool_steelaxe
 link_item hatchetStone default_tool_stoneaxe
 link_item hatchetWood default_tool_woodaxe
@@ -97,7 +114,7 @@ link_item iron_axe default_tool_steelaxe
 link_item iron_boots 3d_armor_inv_boots_steel
 link_item iron_chestplate 3d_armor_inv_chestplate_steel
 link_item iron_helmet 3d_armor_inv_helmet_steel
-link_item iron_hoe farming_hoe_iron
+link_item iron_hoe farming_tool_steelhoe
 link_item iron_ingot default_steel_ingot
 link_item iron_leggings 3d_armor_inv_leggings_steel
 link_item iron_pickaxe default_tool_steelpick
@@ -105,13 +122,11 @@ link_item iron_shovel default_tool_steelshovel
 link_item iron_sword default_tool_steelsword
 link_item paper default_paper
 link_item pickaxeDiamond default_tool_diamondpick
-link_item pickaxeGold default_tool_mesepick
 link_item pickaxeIron default_tool_steelpick
 link_item pickaxeStone default_tool_stonepick
 link_item pickaxeWood default_tool_woodpick
 link_item potion_bottle_empty vessels_glass_bottle_inv
 link_item shovelDiamond default_tool_diamondshovel
-link_item shovelGold default_tool_meseshovel
 link_item shovelIron default_tool_steelshovel
 link_item shovelStone default_tool_stoneshovel
 link_item shovelWood default_tool_woodshovel
@@ -119,27 +134,31 @@ link_item sign default_sign_wall
 link_item snowball default_snowball
 link_item stick default_stick
 link_item stone_axe default_tool_stoneaxe
-link_item stone_hoe farming_hoe_stone
+link_item stone_hoe farming_tool_stonehoe
 link_item stone_pickaxe default_tool_stonepick
 link_item stone_shovel default_tool_stoneshovel
 link_item stone_sword default_tool_stonesword
-link_item string default_string
+link_item string farming_string
 link_item swordDiamond default_tool_diamondsword
-link_item swordGold default_tool_mesesword
 link_item swordIron default_tool_steelsword
 link_item swordStone default_tool_stonesword
 link_item swordWood default_tool_woodsword
+link_item wooden_axe default_tool_woodaxe
 link_item wood_axe default_tool_woodaxe
-link_item wood_hoe farming_hoe_wood
+link_item wooden_hoe farming_tool_woodhoe
+link_item wood_hoe farming_tool_woodhoe
+link_item wooden_pickaxe default_tool_woodpick
 link_item wood_pickaxe default_tool_woodpick
+link_item wooden_shovel default_tool_woodshovel
 link_item wood_shovel default_tool_woodshovel
+link_item wooden_sword default_tool_woodsword
 link_item wood_sword default_tool_woodsword
 
 link_block blockDiamond default_diamond_block
 link_block blockGold default_gold_block
 link_block blockIron default_steel_block
 link_block bookshelf default_bookshelf
-link_block brick default_brick
+link_block bricks default_brick
 link_block cactus_side default_cactus_side
 link_block cactus_top default_cactus_top
 link_block clay default_clay
@@ -194,44 +213,52 @@ link_block gravel default_gravel
 link_block ice default_ice
 link_block iron_block default_steel_block
 link_block iron_ore default_mineral_iron
-link_block ladder default_ladder
+link_block ladder default_ladder_wood
 link_block lava default_lava_source_animated
 link_block lava_flow default_lava_flowing_animated
 link_block lava_still default_lava_source_animated
 link_block leaves default_leaves
 link_block leaves_jungle default_jungleleaves
+link_block oak_leaves default_leaves
 link_block leaves_oak default_leaves
+link_block jungle_log default_jungletree
 link_block log_jungle default_jungletree
+link_block jungle_log_top default_jungletree_top
 link_block log_jungle_top default_jungletree_top
 link_block log_oak default_tree
+link_block oak_log default_tree
 link_block log_oak_top default_tree_top
+link_block oak_log_top default_tree_top
 link_block obsidian default_obsidian
 link_block oreCoal default_mineral_coal
 link_block oreDiamond default_mineral_diamond
 link_block oreGold default_mineral_gold
 link_block oreIron default_mineral_iron
+link_block jungle_planks default_junglewood
 link_block planks_jungle default_junglewood
+link_block oak_planks default_wood
 link_block planks_oak default_wood
-link_block rail default_rail
-link_block rail_normal default_rail
-link_block rail_turn default_rail_curved
+link_block rail carts_rail_straight
+link_block rail_normal carts_rail_straight
+link_block rail_turn carts_rail_curved
 link_block reeds default_papyrus
 link_block sand default_sand
 link_block sandstone_normal default_sandstone
 link_block sandstone_smooth default_sandstone_brick
 link_block sandstone_top default_sandstone
+link_block oak_sapling default_sapling
 link_block sapling default_sapling
+link_block jungle_sapling default_junglesapling
 link_block sapling_jungle default_junglesapling
 link_block snow default_snow
-link_block stonebrick default_cobble
-link_block stonebrick default_stonebrick
+link_block stone_bricks default_stone_brick
 link_block stonebricksmooth default_stone_brick
 link_block stone default_stone
 link_block stonemoss default_mossycobble
 link_block tallgrass default_junglegrass
-link_block tnt_bottom default_tnt_bottom
-link_block tnt_side default_tnt_side
-link_block tnt_top default_tnt_top
+link_block tnt_bottom tnt_bottom
+link_block tnt_side tnt_side
+link_block tnt_top tnt_top
 link_block torch default_torch_on_floor_animated
 link_block torch_on default_torch
 link_block tree_jungle default_jungletree
@@ -254,6 +281,18 @@ link_block wool_colored_pink wool_pink
 link_block wool_colored_red wool_red
 link_block wool_colored_white wool_white
 link_block wool_colored_yellow wool_yellow
+link_block black_wool wool_black
+link_block blue_wool wool_blue
+link_block brown_wool wool_brown
+link_block cyan_wool wool_cyan
+link_block gray_wool wool_grey
+link_block green_wool wool_green
+link_block magenta_wool wool_magenta
+link_block orange_wool wool_orange
+link_block pink_wool wool_pink
+link_block red_wool wool_red
+link_block white_wool wool_white
+link_block yellow_wool wool_yellow
 link_block furnace_top default_furnace_bottom
 
 echo "done!"
